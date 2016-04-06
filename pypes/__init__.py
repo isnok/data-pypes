0410840dc08327c8306b1992f3166f5322c23d36
Konstantin Martini <k@martini.pm>
2016-04-06 23:37:28 +0200

    added setup.py and auto-versioning :-)

--- pypes/__init__.py
+++ pypes/__init__.py
@@ -4,3 +4,6 @@ from .logsetup import setup_logger
 from .pypes import PypeSegment
 from .pypes import PypeLine
 from .pypes import wrap_for_next_segment
+
+from ._version import get_version
+__version__ = get_version()
---
+++ setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+#from distutils.core import setup
+from setuptools import setup
+from setuptools import find_packages
+
+from versioning import get_cmdclass, get_version
+
+import os
+
+# General Info
+
+setup_args = dict(
+    name='data-pypes',
+    version=get_version(),
+    cmdclass=get_cmdclass(),
+    description='A micro framework for processing and logging.',
+    author='Konstantin Martini',
+    author_email='k@tuxcode.org',
+    url='https://github.com/isnok/data-pypes',
+)
+
+
+# Data Files
+
+setup_args.update(
+    packages=find_packages(),
+    zip_safe=False,
+)
+
+
+# Requirements
+
+setup_args.update(
+    install_requires=[],
+)
+
+
+# Entry Points
+
+setup_args.update(
+    entry_points={},
+)
+
+
+# Techincal Details
+
+setup_args.update(
+    keywords=['logging', 'number-crunching'],
+    platforms=['Debian/GNU Linux'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        #"License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: System :: Software Distribution",
+        #"Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+        #"Topic :: Internet",
+        "Intended Audience :: Developers",
+        #"Intended Audience :: System Administrators",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        #"Environment :: Console",
+    ],
+)
+
+setup(**setup_args)
